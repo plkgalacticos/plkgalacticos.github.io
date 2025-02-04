f071f69f8de5eb70ee22b9c83730a28a849e48d9
Petar <pebe1928@gmail.com>
2025-02-04 22:57:56 +0100

    Fixing bg for iphone

--- nextjs/src/app/sections/Join.tsx
+++ nextjs/src/app/sections/Join.tsx
@@ -37,7 +37,7 @@ const Join = ({t}) => {
 
   return (
     <section className='relative  flex flex-col justify-center items-center gap-12 text-center min-h-[100vh]'>
-        <div className='bg-join bg-no-repeat bg-cover bg-fixed bg-center-safari bg-blend-darken bg-black/70 absolute top-0 bottom-0 left-0 right-0'></div>
+        <div className='bg-join bg-no-repeat bg-cover md:bg-fixed bg-center bg-center-safari bg-blend-darken bg-black/70 absolute top-0 bottom-0 left-0 right-0'></div>
         <h1 ref={h1Ref as React.RefObject<HTMLHeadingElement>} className={`slide-top-pre ${h1Animation} text-4xl lg:text-6xl font-semibold`}>{t['h1']} <span className='text-logo-yellow'>{t['h2']}</span> {t['h3']}</h1>
         <h2 ref={h2Ref as React.RefObject<HTMLHeadingElement>} className={`slide-botom-pre ${h2Animation} text-2xl lg:text-3xl font-semibold max-w-lg text-center leading-normal`}>{t['p']}</h2>
         <button ref={btnRef as React.RefObject<HTMLButtonElement>} onClick={showAlert} className={`slide-in-elliptic-top-fwd-pre ${btnAnimation} bg-logo-yellow md:text-lg btn-diagonal-swipe max-md:px-4 max-md:py-2 text-white`}>{t['btn']}</button>
