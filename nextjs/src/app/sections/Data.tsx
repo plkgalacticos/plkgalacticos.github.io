257e9cdd2d1e20572808768a1b41f0bd7132fc68
Petar <pebe1928@gmail.com>
2025-05-11 14:14:21 +0200

    Updating numbers

--- nextjs/src/app/sections/Data.tsx
+++ nextjs/src/app/sections/Data.tsx
@@ -6,22 +6,22 @@ const Data = ({t}) => {
   return (
     <section className='flex flex-col md:flex-row flex-wrap justify-evenly items-center gap-12'>
         <div className='w-full max-w-64 min-h-32 rounded-md gradient-yellow'>
-            <p className='text-3xl lg:text-4xl font-extrabold'><Counter targetValue={30}></Counter></p>
+            <p className='text-3xl lg:text-4xl font-extrabold'><Counter targetValue={34}></Counter></p>
             <p className='text-xl lg:text-2xl'>{t['h1']}</p>
         </div>
 
         <div className='w-full max-w-64 min-h-32 rounded-md gradient-yellow'>
-            <p className='text-3xl lg:text-4xl font-extrabold'><Counter targetValue={82}></Counter></p>
+            <p className='text-3xl lg:text-4xl font-extrabold'><Counter targetValue={100}></Counter></p>
             <p className='text-xl lg:text-2xl'>{t['h2']}</p>
         </div>
 
         <div className='w-full max-w-64 min-h-32 rounded-md gradient-yellow'>
-            <p className='text-3xl lg:text-4xl font-extrabold'><Counter targetValue={13}></Counter></p>
+            <p className='text-3xl lg:text-4xl font-extrabold'><Counter targetValue={17}></Counter></p>
             <p className='text-xl lg:text-2xl'>{t['h3']}</p>
         </div>
 
         <div className='w-full max-w-64 min-h-32 rounded-md gradient-yellow'>
-            <p className='text-3xl lg:text-4xl font-extrabold'><Counter targetValue={10}></Counter></p>
+            <p className='text-3xl lg:text-4xl font-extrabold'><Counter targetValue={13}></Counter></p>
             <p className='text-xl lg:text-2xl'>{t['h4']}</p>
         </div>
     </section>
